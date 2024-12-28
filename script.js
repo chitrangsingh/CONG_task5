aa1f0ab4886f29e9aff180f53af860753035bc7b
CHITRANG SINGH <chitrangsingh225@gmail.com>
2024-12-28 17:01:03 +0530

    slideshow third m final commit

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ var slideIndex = 0;
         }
 
         setInterval(showSlides, 5000);
+
+// here we will take the thumbails and for each thumbnail we will add a EventListener and then we 
+// will get the thumbnails href link of the image and then we will store that into img source of the lightbox and make it visibile over 
+// screen
+
+        document.querySelectorAll(".thumbnail").forEach( thumbnail => {
+            thumbnail.addEventListener("click" , (event) => {
+                event.preventDefault();
+                let imgSRC = thumbnail.getAttribute('href');
+                let lightboxIMG = document.getElementById("lightbox-img");
+                lightboxIMG.src = imgSRC;
+                document.getElementById("lightbox").style.display = "flex";
+            });
+        });
+
+        document.getElementById("close").addEventListener("click",()=>{
+            document.getElementById("lightbox").style.display = "none";
+        });
